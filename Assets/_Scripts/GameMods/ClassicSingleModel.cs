104b2f383752b8c0ae7109540e9db856c17e348e
Yaygun <yusufaygunnn@hotmail.com>
2024-01-31 22:09:57 +0300

    Limit Attempts
    Player has limited Attempts
    //Defeat situation calculated but not implemanted

--- Assets/_Scripts/GameMods/ClassicSingleModel.cs
+++ Assets/_Scripts/GameMods/ClassicSingleModel.cs
@@ -1,8 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class ClassicSingleModel 
 {
-    
+    public int TrialAttempts = 5;
 }
--- Assets/_Scripts/GameMods/ModClassicSingle.cs
+++ Assets/_Scripts/GameMods/ModClassicSingle.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class ModClassicSingle : MonoBehaviour
@@ -16,6 +17,7 @@ public class ModClassicSingle : MonoBehaviour
     void Start()
     {
         EventManager.Instance.EventEndOfTurn += DecideEndOfTurn;
+        Invoke("GoNextTurn", 0.5f);
     }
 
     // Update is called once per frame
@@ -26,6 +28,26 @@ public class ModClassicSingle : MonoBehaviour
 
     private void DecideEndOfTurn()
     {
+        if(HaveTrialAttempts())
+        {
+            GoNextTurn();
+            return;
+        }
+        Defeat();
+    }
+
+    private void Defeat()
+    {
+        Debug.Log("Defeat");
+    }
+    private bool HaveTrialAttempts()
+    {
+        return model.TrialAttempts != 0;
+    }
+
+    private void GoNextTurn()
+    {
+        model.TrialAttempts--;
         EventManager.Instance.PrepareNextTurn();
     }
 
