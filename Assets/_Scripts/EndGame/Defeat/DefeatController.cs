1fc288e4db6ca829b3557ea4f8a973c79f177822
Yaygun <yusufaygunnn@hotmail.com>
2024-02-01 22:17:06 +0300

    Defeat Script Creation
    Empty Script no implementation

---
+++ Assets/_Scripts/EndGame/Defeat/DefeatController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefeatController : MonoBehaviour
+{
+    [SerializeField] private DefeatView View;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
