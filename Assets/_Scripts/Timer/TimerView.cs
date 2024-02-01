9e4e2956518ed7138df9d6fe61dd21015265d933
Yaygun <yusufaygunnn@hotmail.com>
2024-02-01 14:08:56 +0300

    Timer view
    Timer is a prefab and uı element

--- Assets/_Scripts/Timer/TimerView.cs
+++ Assets/_Scripts/Timer/TimerView.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TimerView : MonoBehaviour
 {
-    
+    [SerializeField] private TextMeshProUGUI TimerText;
     public void TimeChanged(int time)
     {
-        Debug.Log("Time : " + time);
+        WriteTime(time);
+    }
+
+
+    private void WriteTime(int time)
+    {
+        TimerText.text = time.ToString();
     }
 }
