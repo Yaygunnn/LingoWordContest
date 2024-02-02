028c81d2a240e34c9757e8292d5b45a6b6e8640a
Yaygun <yusufaygunnn@hotmail.com>
2024-02-02 22:47:54 +0300

    Defeat View Changed
    Defeat opens a panel and writes defeat text

--- Assets/_Scripts/EndGame/Defeat/DefeatView.cs
+++ Assets/_Scripts/EndGame/Defeat/DefeatView.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DefeatView : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject EndResultPanel;
+
+    [SerializeField] private TextMeshProUGUI textMestPro;
+
+    private string DefeatText = "REZÝÝÝLLLLLLLL";
     public void ShowDefeat()
     {
-        Debug.Log("Defeat");
+        EndResultPanel.SetActive(true);
+        textMestPro.text = DefeatText;
     }
 }
