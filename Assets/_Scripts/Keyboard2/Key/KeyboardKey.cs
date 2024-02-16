2e5b1cfd8d3582ef6646de716cf9b45fac3292eb
Yaygun <yusufaygunnn@hotmail.com>
2024-02-16 21:09:35 +0300

    New Key Prefab
    New key for new keyboard

---
+++ Assets/_Scripts/Keyboard2/Key/KeyboardKey.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class KeyboardKey : MonoBehaviour
+{
+    private char key;
+    void Start()
+    {
+        ReadKeyFromTextMeshProAndSetKey();
+    }
+
+    public void ButtonClicked()
+    {
+        KeyManagerController.Instance.KeyPressed(key);
+    }
+    private void ReadKeyFromTextMeshProAndSetKey()
+    {
+        key = GetComponentInChildren<TextMeshProUGUI>().text[0];
+    }
+}
