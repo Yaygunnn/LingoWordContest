85ca928abca313875a40c7f494b584a9f272b3ec
Yaygun <yusufaygunnn@hotmail.com>
2024-03-03 17:24:04 +0300

    built error fix
    these lines prevented built

--- Assets/_Scripts/Timer/TimerController.cs
+++ Assets/_Scripts/Timer/TimerController.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Profiling.HierarchyFrameDataView;
 
 public class TimerController : MonoBehaviour
 {
