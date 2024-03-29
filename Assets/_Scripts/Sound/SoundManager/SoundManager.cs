032c0d3e65da401420f6c2317443dec4958d843c
Yaygun <yusufaygunnn@hotmail.com>
2024-03-29 22:46:50 +0300

    Audio Manager
    Static audio manager implemented

---
+++ Assets/_Scripts/Sound/SoundManager/SoundManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManagerModel model;
+
+    private static SoundManager Instance;
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+            
+            model = GetComponent<SoundManagerModel>();
+        }
+    }
+
+    public static void CellStateSound(E_CellState cellState)
+    {
+        model.CellTrueSound.Play(model.SFXAudioSource);
+    }
+    
+}
---
+++ Assets/_Scripts/Sound/SoundManager/SoundManagerModel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerModel : MonoBehaviour
+{
+    [SerializeField] public AudioSource SFXAudioSource;
+
+
+    [SerializeField] public AudioEvent CellTrueSound;
+}
